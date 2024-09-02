6c366b6751de387d622f6b02da7bf6ea7e1ce3a5
Edwin <moyiedwin8@gmail.com>
2024-09-03 00:31:30 +0300

    class anagram

--- lib/anagram.py
+++ lib/anagram.py
@@ -2,9 +2,9 @@
 class Anagram:
     def __init__(self, word):
         self.word_letters = sorted([letter for letter in word])
-    def match(self, word_list):
+    def match(self, list):
         matches= []
-        for word in word_list:
+        for word in list:
             if sorted([letter for letter in word]) == self.word_letters:
                 matches.append(word)
         return matches
